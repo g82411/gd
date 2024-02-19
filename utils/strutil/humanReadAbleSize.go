5f04b9a3bbcdb4e3f8ab8af0b00a37e4db6a6b25
andy.lin <andy_lin@tomofun.com>
2024-02-20 00:28:20 +0800

    Add HumanReadableSize function in strutil package
    This commit adds a new function, HumanReadableSize, to the strutil package. This function is used to convert size from bytes into a more readable format. It supports displaying size in bytes, kilobytes, megabytes, gigabytes, terabytes, petabytes, and exabytes.

---
+++ utils/strutil/humanReadAbleSize.go
@@ -0,0 +1,17 @@
+package strutil
+
+import "fmt"
+
+func HumanReadAbleSize(size int64) string {
+	const unit = 1024
+	if size < unit {
+		return fmt.Sprintf("%d B", size)
+	}
+	div, exp := int64(unit), 0
+	for n := size / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB",
+		float64(size)/float64(div), "KMGTPE"[exp])
+}
